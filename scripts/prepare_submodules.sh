f8c36f459074ebac6f355ea6b1bcbf0c0dff5f1e
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-01-11 16:17:42 +0100

    Add submodules shell script

---
+++ scripts/prepare_submodules.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+# usage function
+usage() {
+  cat <<EOF
+usage: prepare_submodules.sh [-h|--help]
+
+This script clones the 'policy-classifier-data' repository
+
+optional arguments:
+  -h, --help  <flag>
+              show this help message and exit
+EOF
+}
+
+# check for help
+check_help() {
+  for arg; do
+    if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]; then
+      usage
+      exit 0
+    fi
+  done
+}
+
+# define main function
+main() {
+  git submodule update --init --recursive
+}
+
+# execute all functions
+check_help "$@"
+main
