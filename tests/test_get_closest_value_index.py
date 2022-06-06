e7a4c9e3d2799d220344a74b955ef06bc19ba188
Atreya Shankar <35427332+atreyasha@users.noreply.github.com>
2022-06-06 13:22:29 +0200

    Update unit tests
    1. Split each tested function into individual file
    2. Use pytest parameterize decorator to have simpler representation of
       repeating tests

---
+++ tests/test_get_closest_value_index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from src.train import get_closest_value_index
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize("vector,closest_value,index", [([1., 2., 3.], 1., 0),
+                                                        ([1., 2., 3.], 2., 1),
+                                                        ([1., 2., 3.], 3., 2),
+                                                        ([1., 2., 3.], 1.5, 0),
+                                                        ([1., 2., 3.], 2.5, 1),
+                                                        ([1., 2., 3.], 3.5, 2),
+                                                        ([1., 1., 2.], 1., 0),
+                                                        ([1., 2., 1.], 1., 0),
+                                                        ([1., 1., 1.], 1., 0)])
+def test_get_closest_value_index(vector, closest_value, index):
+    assert get_closest_value_index(np.array(vector), closest_value) == index
---
+++ tests/test_preprocess.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from src.train import preprocess
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input_document,output_document",
+    [("", ""), ("my website is https://example.com", "my website is "),
+     ("my website is http://example.com", "my website is "),
+     ("my email is person@example.com", "my email is person"),
+     (".%&$", "    "),
+     ("my email is person @ example dot com",
+      "my email is person at example dot com"),
+     ("what a\nnice day", "what a nice day"),
+     ("WHAT A NICE DAY", "what a nice day")])
+def test_preprocess(input_document, output_document):
+    assert preprocess(input_document) == output_document
